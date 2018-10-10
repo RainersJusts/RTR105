48dc4f307260a63b6b02f236a88fcd219b3ac44b
Rainers <justs1999@gmail.com>
2018-10-10 13:37:13 +0300

    20181010_1337

---
+++ dgr_20181010.py
@@ -0,0 +1,242 @@
+Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+[GCC 7.3.0] on linux
+Type "copyright", "credits" or "license()" for more information.
+>>> x = 12.2
+>>> y = 14
+>>> x = 100
+>>> x
+100
+>>> print(123)
+123
+>>> print(x)
+100
+>>> spam23=vantage
+Traceback (most recent call last):
+  File "<pyshell#6>", line 1, in <module>
+    spam23=vantage
+NameError: name 'vantage' is not defined
+>>> spam=23
+>>> spam
+23
+>>> print(spam)
+23
+>>> _speed=4004
+>>> _spped
+Traceback (most recent call last):
+  File "<pyshell#11>", line 1, in <module>
+    _spped
+NameError: name '_spped' is not defined
+>>> _speed
+4004
+>>> SPam
+Traceback (most recent call last):
+  File "<pyshell#13>", line 1, in <module>
+    SPam
+NameError: name 'SPam' is not defined
+>>> SPam=24
+>>> SPam
+24
+>>> 23spam
+SyntaxError: invalid syntax
+>>> var.12
+SyntaxError: invalid syntax
+>>> x1q3z9ocd = 35.0
+>>> x1q3z9ocd
+35.0
+>>> x1q3z9afd = 12.50
+>>> x1q3p9afd = x1q3z9ocd * x1q3z9afd
+>>> x1q3p9afd
+437.5
+>>> x = 3
+>>> x = x + 3
+>>> x
+6
+>>> print(x)
+6
+>>> x = 3.9*x*(1-x)
+>>> x
+-117.0
+>>> x = 3.9 * x * ( 1 - x )
+>>> x
+-53843.4
+>>> x
+-53843.4
+>>> x = 0.6
+>>> x
+0.6
+>>> x = 3.9*x*(1-x)
+>>> x
+0.9359999999999999
+>>> xx = 2
+>>> xx
+2
+>>> x
+0.9359999999999999
+>>> xx
+2
+>>> xx = xx + 2
+>>> xx
+4
+>>> print(xx)
+4
+>>> yy = 530 * 12
+>>> yy
+6360
+>>> jj = 30
+>>> kk = jj % 33
+>>> print(kk)
+30
+>>> kk = jj % 3
+>>> kk
+0
+>>> kk = jj % 5
+>>> kk
+0
+>>> type(4.)
+<class 'float'>
+>>> type(4)
+<class 'int'>
+>>> type(x)
+<class 'float'>
+>>> x
+0.9359999999999999
+>>> kk = jj % 19
+>>> kk
+11
+>>> print(2**5)
+32
+>>> x = 1 + 2**3/4*5
+>>> x
+11.0
+>>> type(x)
+<class 'float'>
+>>> type(A)
+Traceback (most recent call last):
+  File "<pyshell#62>", line 1, in <module>
+    type(A)
+NameError: name 'A' is not defined
+>>> type(a)
+Traceback (most recent call last):
+  File "<pyshell#63>", line 1, in <module>
+    type(a)
+NameError: name 'a' is not defined
+>>> a = A
+Traceback (most recent call last):
+  File "<pyshell#64>", line 1, in <module>
+    a = A
+NameError: name 'A' is not defined
+>>> z = F
+Traceback (most recent call last):
+  File "<pyshell#65>", line 1, in <module>
+    z = F
+NameError: name 'F' is not defined
+>>> a = 'A'
+>>> a
+'A'
+>>> type(a)
+<class 'str'>
+>>> a + 1
+Traceback (most recent call last):
+  File "<pyshell#69>", line 1, in <module>
+    a + 1
+TypeError: must be str, not int
+>>> a + 'B'
+'AB'
+>>> i = 42
+>>> f = float(i)
+>>> f
+42.0
+>>> type(f)
+<class 'float'>
+>>> type(i)
+<class 'int'>
+>>> print(9/2)
+4.5
+>>> print(10/2)
+5.0
+>>> int(print(10/2))
+5.0
+Traceback (most recent call last):
+  File "<pyshell#78>", line 1, in <module>
+    int(print(10/2))
+TypeError: int() argument must be a string, a bytes-like object or a number, not 'NoneType'
+>>> sval='123'
+>>> sval
+'123'
+>>> type(sval)
+<class 'str'>
+>>> ival = int(sval)
+>>> ival
+123
+>>> type(ival)
+<class 'int'>
+>>> nsv = 'hello'
+>>> niv = int(nsv)
+Traceback (most recent call last):
+  File "<pyshell#86>", line 1, in <module>
+    niv = int(nsv)
+ValueError: invalid literal for int() with base 10: 'hello'
+>>> nam = input("Who are you?")
+Who are you?print('Welcome', nam)
+>>> nam
+"print('Welcome', nam)"
+>>> nam = input("Who are you?") print('Welcome', nam)
+SyntaxError: invalid syntax
+>>> nam = input("Who are you?")
+Who are you?Rainers
+>>> print('Welcome', nam)
+Welcome Rainers
+>>> inp = input('Europe floor?')
+Europe floor?0
+>>> usf = int(inp) + 1
+>>> print('US floor', usf)
+US floor 1
+>>> #this is a test
+>>> input('Enter hours' svf)
+SyntaxError: invalid syntax
+>>>  input('Enter hours', svf)
+SyntaxError: unexpected indent
+>>> input('Enter hours', svf)
+Traceback (most recent call last):
+  File "<pyshell#98>", line 1, in <module>
+    input('Enter hours', svf)
+NameError: name 'svf' is not defined
+>>> svf = input('Enter hours')
+Enter hours35
+>>> fvs = input('Enter rate:')
+Enter rate:2.75
+>>> svf
+'35'
+>>> vfs
+Traceback (most recent call last):
+  File "<pyshell#102>", line 1, in <module>
+    vfs
+NameError: name 'vfs' is not defined
+
+>>> fvs
+'2.75'
+>>> print('pay', svf*vfs)
+Traceback (most recent call last):
+  File "<pyshell#104>", line 1, in <module>
+    print('pay', svf*vfs)
+NameError: name 'vfs' is not defined
+>>> print('pay', svf*fvs)
+Traceback (most recent call last):
+  File "<pyshell#105>", line 1, in <module>
+    print('pay', svf*fvs)
+TypeError: can't multiply sequence by non-int of type 'str'
+>>> isvf = int(svf)
+>>> ifvs = int(fvs)
+Traceback (most recent call last):
+  File "<pyshell#107>", line 1, in <module>
+    ifvs = int(fvs)
+ValueError: invalid literal for int() with base 10: '2.75'
+>>> ifvs = float(fvs)
+>>> print('pay', isvf*ivfs)
+Traceback (most recent call last):
+  File "<pyshell#109>", line 1, in <module>
+    print('pay', isvf*ivfs)
+NameError: name 'ivfs' is not defined
+>>> print('pay', isvf*ifvs)
+pay 96.25
+>>> 
