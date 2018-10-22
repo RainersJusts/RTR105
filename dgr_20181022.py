24c47ea9e7506fd9aec48a02d8f0e54b8dd69eb7
Rainers <justs1999@gmail.com>
2018-10-22 17:12:39 +0300

    20181022_1713

---
+++ dgr_20181022.py
@@ -0,0 +1,44 @@
+##def addition(a, b):
+##    add = a + b
+##    return add
+##
+##xs = input('Pirmais skaitlis: ')
+##try:
+##    x = int(xs)
+##except:
+##    try:
+##        x = float(xs)
+##    except:
+##        print('Invalid character')
+##ys = input('Otrais skaitlis: ')
+##y = float(ys)
+##xy = addition(x, y)
+##print(xy)
+##
+##big = max('Hello world')
+##sml = min('Hello world')
+##print(big)
+##print(sml)
+##
+##print(type(xs))
+##xf = int(xs)
+##print(type(xf))
+##
+##def printtext():
+##    print("And on that one fateful day, The Shadow cast it's power unto the world")
+##    print("Not all was lost though.")
+##    print('The very next day, Light came and scared The Shadow away.')
+##
+##print(printtext())
+##
+##def greet(lang):
+##    if lang == 'es':
+##        return 'Hola'
+##    elif lang == 'fr':
+##        return 'Bonjour'
+##    else:
+##        return 'Hello'
+##
+##print(greet('en'),'Glenn')
+##print(greet('es'),'Sally')
+##print(greet('fr'),'Micheal')
