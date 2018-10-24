374180c65d5916bcbd5e042bc715c40a2b207389
Rainers <justs1999@gmail.com>
2018-10-24 13:52:40 +0300

    20181024_1352

---
+++ dgr_20181024.py
@@ -0,0 +1,92 @@
+##sn = input('Enter Blast off counter start number: ')
+##n = int(sn)
+##while n > 0:
+##    print(n)
+##    n = n - 1
+##print('Blastoff!')
+##print(n)
+##
+##n = 5
+##while n > 0:
+##    print('Lather')
+##    print('Rinse')
+##    break
+##print('Dry off!')
+##
+##
+##while True:
+##    line = input('>')
+##    if line[0] == '#':
+##        continue
+##    if line == 'done':
+##        break
+##    print(line)
+##print('Done!')
+##
+##
+##for i in [5, 4, 3, 2, 1]:
+##    print(i)
+##print('Blastoff!')
+##
+##friends = ['Ieva', 'Elīna', 'Artūrs']
+##for friend in friends:
+##    print('Lai tev jauka diena, ', friend[:-1])
+##print(':)')
+##
+##for i in [5, 4, 3, 2, 1]:
+##    print(i)
+##print('Blastoff!')
+##
+##largest_so_far =-1
+##print('Before', largest_so_far)
+##for the_num in []:
+##    if the_num > largest_so_far:
+##        largest_so_far = the_num
+##    print(largest_so_far, the_num)
+##    
+##print('After', largest_so_far)
+##
+##count = 0
+##sum = 0
+##print('Before', count, sum)
+##for value in [9, 41, 12, 3, 74, 15]:
+##    count = count + 1
+##    sum = sum + value
+##    print(count, sum, value)
+##print('After', count, sum, sum/count)
+##
+##print('Before')
+##for value in [9, 41, 12, 3, 74, 15]:
+##    if value >20:
+##        print('Large number',value)
+##print('After')
+##
+##found = False
+##print('Before', found)
+##for value in [9, 41, 12, 3, 74, 15]:
+##    if value == 3:
+##        found = True
+##    print(found,value)
+##print('After', found)
+##smallest_so_far =-1
+##print('Before', smallest_so_far)
+##for the_num in [9, 41, 12, 3, 74, 15]:
+##    if the_num < smallest_so_far:
+##        smallest_so_far = the_num
+##    print(smallest_so_far, the_num)
+##print('After', smallest_so_far)
+##smallest = None
+##print('Before')
+##for value in [9, 41, 12, 3, 74, 15]:
+##    if smallest is None:
+##        smallest = value
+##    elif value < smallest:
+##        smallest = value
+##    print(smallest, value)
+##print('After', smallest)
+##
+
+
+
+
+
