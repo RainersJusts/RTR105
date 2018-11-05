e2aefa27f589690556f6db873947a5ff227fb324
Rainers <justs1999@gmail.com>
2018-11-05 17:12:56 +0200

    20181105_1712

---
+++ dgr_20181105.py
@@ -0,0 +1,93 @@
+##Python 3.6.5 (default, Apr  1 2018, 05:46:30) 
+##[GCC 7.3.0] on linux
+##Type "copyright", "credits" or "license()" for more information.
+##>>> 
+##================= RESTART: /home/user/RTR105/dgr_20181105.py =================
+##>>> fruit = "banana"
+##>>> print(len(fruit))
+##6
+##>>> print(fruit[2])
+##n
+##>>> x = 4
+##\
+##>>> x = 4
+##>>> print(fruit[x-1])
+##a
+##>>> 
+##
+##fruit = "banana"
+##i = 0
+##while i < len(fruit):
+##    l = fruit[i]
+##    print(i, l)
+##    i = i + 1
+##
+##fruit = "banana"
+##for letter in fruit:
+##    print(letter)
+##    
+##f = 'banana'
+##count = 0
+##for letter in f:
+##    if letter == 'n':
+##        count = count + 1
+##print(count)
+##
+##for letter in "banana":
+##    print(letter)
+##
+##s = "Johnny Eagle"
+##print(s[0:4])
+##print(s[:7])
+##print(s[6:])
+##print(s[:])
+##
+##a = 'Everybody'
+##b = a + "has to raise their hand"
+##print(b)
+##c = a + " " + "has to raise their hand"
+##print(c)
+##
+##fruit = 'Daniels'
+##print('n' in fruit)
+##print("m" in fruit)
+##print('ani' in fruit)
+##if 'a' in fruit:
+##    print("Found it!")
+##
+##word = input('Insert a fruits name')
+##if word < "Daniels":
+##    print('Your word,' + word + ', comes before fruits')
+##elif word > "Daniels":
+##    print('Your word,' + word + ', comes after fruits')
+##else:
+##    print('All right, fruits!')
+##
+##groot = "I am Groot"
+##zap = groot.lower()
+##print(zap)
+##print(groot)
+##print('Hi there, Groot!'.lower())
+##
+##sup = 'SUpp'
+##print(type(sup))
+##print(sup.lower())
+##print(sup.upper())
+##print(sup.swapcase())
+##print(sup.strip())
+##
+##fruit = "banana"
+##pos = fruit.find('na')
+##print(pos)
+##aa = fruit.find("z")
+##print(aa)
+##
+##groot = "I am Groot"
+##nstr = groot.replace('Groot', "here")
+##print(nstr)
+##ntsr = groot.replace('o',"X")
+##print(ntsr)
+##print(groot.startswith('I am'))
+##print(groot.startswith('i'))
+
+
