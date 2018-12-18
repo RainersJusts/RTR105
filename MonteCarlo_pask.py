15a4ac3c1b60e642961ef3359f2c84665a4a4a32
Rainers <justs1999@gmail.com>
2018-12-18 15:27:00 +0200

    20181218_1526

--- MonteCarlo_pask.py
+++ MonteCarlo_pask.py
@@ -1,16 +1,26 @@
 import sys
 sys.path.append('/usr/local/anaconda3/lib/python3.6/site-packages')
 
-from numpy import random
+# N vienmieriigi sadaliiti gadiijuma skaitlji
+# N uniformly distributed random numbers
 
-N = 10000
+from numpy import random
+#print(random.__doc__)
+#print(random.uniform.__doc__)
+
+N = 5000 #veelamo punktu skaits
 a = 0
 b = 5
 b1 = 2
 
-x = random.uniform(a,b,N)
+#pseido-gadiijuma skaitlju generatora grauds
+#random.seed(1)
 
-k = [0, 0, 0, 0, 0]
+x = random.uniform(a,b,N)
+#x = random.normal(a,b,N)
+
+"""
+k = [0, 0, 0, 0, 0, 0, 0]
 for i in range(N):
     if x[i] < 1:
         k[0] = k[0] + 1
@@ -20,9 +30,14 @@ for i in range(N):
         k[2] = k[2] + 1
     elif x[i] < 4:
         k[3] = k[3] + 1
-    else:
+    elif x[i] < 5:
         k[4] = k[4] + 1
+    elif x[i] < 6:
+        k[5] = k[5] + 1
+    else:
+        k[6] = k[6] + 1
 print(k)
+"""
 
 from math import cos
 
@@ -33,6 +48,7 @@ plt.grid()
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Funkcija un taas integraalis (laukums starp funkciju un x ass)')
+#nav jeegas ziimeet shaadi plt.plot(x,y)5
 plt.plot(x,y,'ko')
 N1 = 0
 for i in range(N):
